75927a5b7bad7d21f1fcabfa0fb740eabad1bc35
karneasy <gavinmrainey@gmail.com>
2024-03-15 15:05:49 +0000

    Update preparebody.py

--- preparebody.py
+++ preparebody.py
@@ -3,28 +3,27 @@ import json
 from settings import DATA_SAVE_PATH
 
 def replace_string_in_actions(data, search_string, replacement_string):
-    for entry in data:
+    for entry in data.get("matching_records", []):
         for action in entry.get("actions", []):
-            if isinstance(action.get("value"), list):
-                for i, value in enumerate(action.get("value")):
-                    if search_string in value:
-                        action["value"][i] = value.replace(search_string, replacement_string)
+            for i, value in enumerate(action.get("value", [])):
+                if search_string in value:
+                    action["value"][i] = value.replace(search_string, replacement_string)
 
 def main():
     search_string = "Ticket {{ticket.id}} Transferred from Zendesk"
     replacement_string = "{{ticket.title}}"
 
-    macros_file_path = os.path.join(DATA_SAVE_PATH, "macros.json")
-    with open(macros_file_path, "r") as file:
+    matching_records_file_path = os.path.join(DATA_SAVE_PATH, "matching_records.json")
+    with open(matching_records_file_path, "r") as file:
         data = json.load(file)
 
     replace_string_in_actions(data, search_string, replacement_string)
 
-    updated_macros_file_path = os.path.join(DATA_SAVE_PATH, "updated_macros.json")
-    with open(updated_macros_file_path, "w") as outfile:
+    updated_matching_records_file_path = os.path.join(DATA_SAVE_PATH, "updated_matching_records.json")
+    with open(updated_matching_records_file_path, "w") as outfile:
         json.dump(data, outfile, indent=4)
 
-    print("String replaced and saved to updated_macros.json")
+    print("String replaced and saved to updated_matching_records.json")
 
 if __name__ == "__main__":
     main()
